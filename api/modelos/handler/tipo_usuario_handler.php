62e33023fbf9b816f6383f0dc0252a6867489cdb
RichardHerna <20220194@ricaldone.edu.sv>
2024-05-14 10:02:22 -0600

    Crud de tipo usuario

---
+++ api/modelos/handler/tipo_usuario_handler.php
@@ -0,0 +1,83 @@
+<?php
+// Se incluye la clase para trabajar con la base de datos.
+require_once('../../ayudantes/base_datos.php');
+/*
+ *  Clase para manejar el comportamiento de los datos de la tabla CATEGORIA.
+ */
+class Tipo_usuario_handler
+{
+    /*
+     *  Declaración de atributos para el manejo de datos.
+     */
+    protected $id = null;
+    protected $nombre = null;
+    protected $descripcion = null;
+
+    // Constante para establecer la ruta de las imágenes.
+    // const RUTA_IMAGEN = '../../imagenes/categorias';
+
+    /*
+     *  Métodos para realizar las operaciones SCRUD (search, create, read, update, and delete).
+     */
+    public function searchRows()
+    {
+        $value = '%' . Validator::getSearchValue() . '%';
+        $sql = 'SELECT id_tipo, tipo_usuario, descripcion_usuario
+                FROM tb_tipousuarios
+                WHERE tipo_usuario LIKE ?
+                ORDER BY id_tipo';
+        $params = array($value);
+        return Database::getRows($sql, $params);
+    }
+
+    public function createRow()
+    {
+        $sql = 'INSERT INTO tb_tipousuarios(tipo_usuario, descripcion_usuario)
+                VALUES(?, ?)';
+        $params = array($this->nombre, $this->descripcion);
+        return Database::executeRow($sql, $params);
+    }
+
+    public function readAll()
+    {
+        $sql = 'SELECT id_tipo, tipo_usuario, descripcion_usuario
+                FROM tb_tipousuarios
+                ORDER BY id_tipo';
+        return Database::getRows($sql);
+    }
+
+    public function readOne()
+    {
+        $sql = 'SELECT id_tipo, tipo_usuario, descripcion_usuario
+                FROM tb_tipousuarios
+                WHERE id_tipo = ?';
+        $params = array($this->id);
+        return Database::getRow($sql, $params);
+    }
+
+    // public function readFilename()
+    // {
+    //     $sql = 'SELECT imagen_categoria
+    //             FROM categoria
+    //             WHERE id_categoria = ?';
+    //     $params = array($this->id);
+    //     return Database::getRow($sql, $params);
+    // }
+
+    public function updateRow()
+    {
+        $sql = 'UPDATE tb_tipousuarios
+                SET tipo_usuario = ?, descripcion_usuario = ?
+                WHERE id_tipo = ?';
+        $params = array($this->nombre, $this->descripcion, $this->id);
+        return Database::executeRow($sql, $params);
+    }
+
+    public function deleteRow()
+    {
+        $sql = 'DELETE FROM tb_tipousuarios
+                WHERE id_tipo = ?';
+        $params = array($this->id);
+        return Database::executeRow($sql, $params);
+    }
+}
--- controladores/administrador/tiposUsuarios.js
+++ controladores/administrador/tiposUsuarios.js
@@ -1,5 +1,5 @@
 // Constante para completar la ruta de la API.
-const CATEGORIA_API = 'services/admin/categoria.php';
+const TIPO_USUARIO_API = 'servicios/administrador/tipo_usuario.php';
 // Constante para establecer el formulario de buscar.
 const SEARCH_FORM = document.getElementById('searchForm');
 // Constantes para establecer los elementos de la tabla.
@@ -10,10 +10,9 @@ const SAVE_MODAL = new bootstrap.Modal('#saveModal'),
     MODAL_TITLE = document.getElementById('modalTitle');
 // Constantes para establecer los elementos del formulario de guardar.
 const SAVE_FORM = document.getElementById('saveForm'),
-    ID_CATEGORIA = document.getElementById('idCategoria'),
-    NOMBRE_CATEGORIA = document.getElementById('nombreCategoria'),
-    DESCRIPCION_CATEGORIA = document.getElementById('descripcionCategoria'),
-    IMAGEN_CATEGORIA = document.getElementById('imagenCategoria');
+    ID_TIPO = document.getElementById('id_tipo'),
+    TIPO_NOMBRE = document.getElementById('tipo_nombre'),
+    DESCRIPCION_TIPO = document.getElementById('descripcion_tipo');
 
 // Método del evento para cuando el documento ha cargado.
 document.addEventListener('DOMContentLoaded', () => {
@@ -40,11 +39,11 @@ SAVE_FORM.addEventListener('submit', async (event) => {
     // Se evita recargar la página web después de enviar el formulario.
     event.preventDefault();
     // Se verifica la acción a realizar.
-    (ID_CATEGORIA.value) ? action = 'updateRow' : action = 'createRow';
+    (ID_TIPO.value) ? action = 'updateRow' : action = 'createRow';
     // Constante tipo objeto con los datos del formulario.
     const FORM = new FormData(SAVE_FORM);
     // Petición para guardar los datos del formulario.
-    const DATA = await fetchData(CATEGORIA_API, action, FORM);
+    const DATA = await fetchData(TIPO_USUARIO_API, action, FORM);
     // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
     if (DATA.status) {
         // Se cierra la caja de diálogo.
@@ -70,7 +69,7 @@ const fillTable = async (form = null) => {
     // Se verifica la acción a realizar.
     (form) ? action = 'searchRows' : action = 'readAll';
     // Petición para obtener los registros disponibles.
-    const DATA = await fetchData(CATEGORIA_API, action, form);
+    const DATA = await fetchData(TIPO_USUARIO_API, action, form);
     // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
     if (DATA.status) {
         // Se recorre el conjunto de registros fila por fila.
@@ -78,17 +77,17 @@ const fillTable = async (form = null) => {
             // Se crean y concatenan las filas de la tabla con los datos de cada registro.
             TABLE_BODY.innerHTML += `
                 <tr>
-                    <td><img src="${SERVER_URL}images/categorias/${row.imagen_categoria}" height="50"></td>
-                    <td>${row.nombre_categoria}</td>
-                    <td>${row.descripcion_categoria}</td>
+                    <td>${row.id_tipo}</td>
+                    <td>${row.tipo_usuario}</td>
+                    <td>${row.descripcion_usuario}</td>
                     <td>
-                        <button type="button" class="btn btn-info" onclick="openUpdate(${row.id_categoria})">
+                        <button type="button" class="btn btn-info" onclick="openUpdate(${row.id_tipo})">
                             <i class="bi bi-pencil-fill"></i>
                         </button>
-                        <button type="button" class="btn btn-danger" onclick="openDelete(${row.id_categoria})">
+                        <button type="button" class="btn btn-danger" onclick="openDelete(${row.id_tipo})">
                             <i class="bi bi-trash-fill"></i>
                         </button>
-                        <button type="button" class="btn btn-warning" onclick="openReport(${row.id_categoria})">
+                        <button type="button" class="btn btn-warning" onclick="openReport(${row.id_tipo})">
                             <i class="bi bi-filetype-pdf"></i>
                         </button>
                     </td>
@@ -123,9 +122,9 @@ const openCreate = () => {
 const openUpdate = async (id) => {
     // Se define una constante tipo objeto con los datos del registro seleccionado.
     const FORM = new FormData();
-    FORM.append('idCategoria', id);
+    FORM.append('id_tipo', id);
     // Petición para obtener los datos del registro solicitado.
-    const DATA = await fetchData(CATEGORIA_API, 'readOne', FORM);
+    const DATA = await fetchData(TIPO_USUARIO_API, 'readOne', FORM);
     // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
     if (DATA.status) {
         // Se muestra la caja de diálogo con su título.
@@ -135,9 +134,9 @@ const openUpdate = async (id) => {
         SAVE_FORM.reset();
         // Se inicializan los campos con los datos.
         const ROW = DATA.dataset;
-        ID_CATEGORIA.value = ROW.id_categoria;
-        NOMBRE_CATEGORIA.value = ROW.nombre_categoria;
-        DESCRIPCION_CATEGORIA.value = ROW.descripcion_categoria;
+        ID_TIPO.value = ROW.id_tipo;
+        TIPO_NOMBRE.value = ROW.tipo_usuario;
+        DESCRIPCION_TIPO.value = ROW.descripcion_usuario;
     } else {
         sweetAlert(2, DATA.error, false);
     }
@@ -155,9 +154,9 @@ const openDelete = async (id) => {
     if (RESPONSE) {
         // Se define una constante tipo objeto con los datos del registro seleccionado.
         const FORM = new FormData();
-        FORM.append('idCategoria', id);
+        FORM.append('id_tipo', id);
         // Petición para eliminar el registro seleccionado.
-        const DATA = await fetchData(CATEGORIA_API, 'deleteRow', FORM);
+        const DATA = await fetchData(TIPO_USUARIO_API, 'deleteRow', FORM);
         // Se comprueba si la respuesta es satisfactoria, de lo contrario se muestra un mensaje con la excepción.
         if (DATA.status) {
             // Se muestra un mensaje de éxito.

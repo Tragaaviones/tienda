5e66d85d632a3833e6c8eb096456bafbb64bc4ba
alvaromonterrosa <monterrosaalvaro35qgmail.com>
2024-06-04 22:50:46 -0600

    produc-php

---
+++ api/servicios/publico/producto.php
@@ -0,0 +1,64 @@
+<?php
+// Se incluye la clase del modelo.
+require_once('../../modelos/data/productos_data.php');
+
+// Se comprueba si existe una acción a realizar, de lo contrario se finaliza el script con un mensaje de error.
+if (isset($_GET['action'])) {
+    // Se crea una sesión o se reanuda la actual para poder utilizar variables de sesión en el script.
+    session_start();
+    // Se instancia la clase correspondiente.
+    $producto = new ProductoData;
+    // Se declara e inicializa un arreglo para guardar el resultado que retorna la API.
+    $result = array('status' => 0, 'session' => 0, 'recaptcha' => 0, 'message' => null, 'error' => null, 'exception' => null, 'username' => null);
+    // Se verifica si existe una sesión iniciada como cliente para realizar las acciones correspondientes.
+    if (isset($_SESSION['id_producto'])) {
+        $result['session'] = 1;
+        // Se compara la acción a realizar cuando un cliente ha iniciado sesión.
+        switch ($_GET['action']) {
+            case 'getUser':
+                if (isset($_SESSION['nombre_producto'])) {
+                    $result['precio_unitario'] = $_SESSION['precio_unitario'];;
+                    $result['descripcion'] = $_SESSION['descripcion'];
+                    $result['foto'] = $_SESSION['imagen'];
+                } else {
+                    $result['error'] = 'Correo de usuario indefinido';
+                }
+                break;
+            case 'logOut':
+                if (session_destroy()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Sesión eliminada correctamente';
+                } else {
+                    $result['error'] = 'Ocurrió un problema al cerrar la sesión';
+                }
+                break;
+            default:
+                $result['error'] = 'Acción no disponible dentro de la sesión';
+        }
+    } else {
+        // Se compara la acción a realizar cuando el cliente no ha iniciado sesión.
+        switch ($_GET['action']) {
+            case 'logIn':
+                $_POST = Validator::validateForm($_POST);
+                if (!$cliente->checkUser($_POST['email'], $_POST['password'])) {
+                    $result['error'] = 'Datos incorrectos';
+                } elseif ($cliente->checkStatus()) {
+                    $result['status'] = 1;
+                    $result['message'] = 'Autenticación correcta';
+                } else {
+                    $result['error'] = 'La cuenta ha sido desactivada';
+                }
+                break;
+            default:
+                $result['error'] = 'Acción no disponible fuera de la sesión';
+        }
+    }
+    // Se obtiene la excepción del servidor de base de datos por si ocurrió un problema.
+    $result['exception'] = Database::getException();
+    // Se indica el tipo de contenido a mostrar y su respectivo conjunto de caracteres.
+    header('Content-type: application/json; charset=utf-8');
+    // Se imprime el resultado en formato JSON y se retorna al controlador.
+    print(json_encode($result));
+} else {
+    print(json_encode('Recurso no disponible'));
+}
